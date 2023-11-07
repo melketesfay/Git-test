650404ae09e094534cb365606a68cecc5622fd2f
melketesfay <melke.tesfay@gmail.com>
2023-11-07 11:57:27 +0100

    added a function to test.py

--- test.py
+++ test.py
@@ -4,3 +4,13 @@ print("just a test for an emergency fix")
 
 
 print("just a test for remote repo")
+
+number = int(input("calculate factorial of:"))
+
+def factorial(n):
+	if(n <= 1):
+		return 1
+	else:
+		return n*factorial(n-1)
+
+print(factorial(number))
